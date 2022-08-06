97a3b890abf45f40f35d4d90aab8b7fa47bc8262
anikinkirill <anikinkirill@yandex-team.ru>
2022-08-06 22:43:05 +0300

    Handle ViewControllers

---
+++ TodoApp/TaskViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TaskViewController.swift
+//  TodoApp
+//
+//  Created by Kirill Anikin on 06.08.2022.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -8,13 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // Some more code here
+        self.title = "Tasks"
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        if !UserDefaults().bool(forKey: "setup") {
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+        }
+        updateTasks()
     }
-
+    
+    private func updateTasks() {
+        tasks.removeAll()
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        for i in 0..<count {
+            if let task = UserDefaults().value(forKey: "task_\(i+1)") as? String {
+                tasks.append(task)
+            }
+        }
+        tableView.reloadData()
+    }
+    
+    @IBAction private func didTapAdd() {
+        let viewController = storyboard?.instantiateViewController(withIdentifier: "entry") as! EntryViewController
+        viewController.title = "New Task"
+        viewController.update = {
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+        }
+        navigationController?.pushViewController(viewController, animated: true)
+    }
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+}
